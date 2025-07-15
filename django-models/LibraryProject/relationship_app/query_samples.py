f0be9a068dbae654583cc08b505435dbbb1bc005
David Akinola <dakinola54@gmail.com>
2025-07-15 20:22:51 +0100

    User a variable for library name

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,13 +1,14 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 def main():
+    library_name = "City Library"
     author = Author.objects.get(name='author_name')
     books_by_author = Book.objects.filter(author=author)
     print("Books by John Doe:")
     for book in books_by_author:
         print(f"- {book.title}")
 
-    library = Library.objects.get(name='library_name')
+    library = Library.objects.get(name=library_name)
     books_in_library = library.books.all()
     print("\nBooks in City Library:")
     for book in books_in_library:
