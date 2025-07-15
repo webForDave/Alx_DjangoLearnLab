3e2415515e3c3234e5ffec205d0e64cd1eec83e4
David Akinola <dakinola54@gmail.com>
2025-07-15 20:11:13 +0100

    Implemented dunder for Author model

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     Author = models.ForeignKey(Author, on_delete=models.CASCADE)
