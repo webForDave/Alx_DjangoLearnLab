805a93428510da88bbaae9f648199be0f3a40d96
David Akinola <dakinola54@gmail.com>
2025-07-16 19:32:34 +0100

    Replaced generic.DetailView with views.generics.detail.DetailView

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from .models import Book
 from .models import Library
-from django.views import generic
+from django.views.generic.detail import DetailView
 
 def all_books(request):
     context = {"books": Book.objects.all()}
     return render(request, "relationship_app/list_books.html", context)
 
-class BookDetail(generic.DetailView):
+class BookDetail(DetailView):
     template_name = "relationship_app/library_detail.html"
     model = Library
