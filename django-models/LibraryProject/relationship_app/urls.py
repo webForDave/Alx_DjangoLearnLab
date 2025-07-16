8e35024a17113bfb708b5a2cab4982b241a6ccff
David Akinola <dakinola54@gmail.com>
2025-07-16 19:29:02 +0100

    Class for adding books and a library

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import all_books
+from .views import all_books, BookDetail
 
 urlpatterns = [
     path("books/", all_books, name="Books"),
+    path("books/<int:pk>/", BookDetail.as_view(), name="Book detail"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Book, Library
+from django.views import generic
 
 def all_books(request):
     context = {"books": Book.objects.all()}
     return render(request, "relationship_app/list_books.html", context)
+
+class BookDetail(generic.DetailView):
+    template_name = "relationship_app/library_detail.html"
+    model = Library
