e0c82d8b5acea1c40d069afc4aff906369a12138
David Akinola <dakinola54@gmail.com>
2025-07-17 18:17:59 +0100

    Django basic authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, login_view, logout_view, register_view
 
 urlpatterns = [
-    path("books/", list_books, name="Books"),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
+    path("register/", register_view, name="register"),
+    path("books/", list_books, name="list_books"),
     path("books/<int:pk>/", LibraryDetailView.as_view(), name="Book detail"),
 ]
