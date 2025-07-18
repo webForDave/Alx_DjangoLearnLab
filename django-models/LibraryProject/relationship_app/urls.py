50ee30b664b2147e4a0fe65b191fcd896aeebcff
David Akinola <dakinola54@gmail.com>
2025-07-18 18:57:02 +0100

    Checker passes auth error

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import register, logout_view, list_books, LibraryDetailView
+from . import views
 
 urlpatterns = [
-    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
-    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
-    path("register/", register, name="register"),
-    path("books/", list_books, name="list_books"),
-    path("books/<int:pk>/", LibraryDetailView.as_view(), name="Book detail"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("books/", views.list_books, name="list_books"),
+    path("books/<int:pk>/", views.LibraryDetailView.as_view(), name="Book detail"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,23 +1,35 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 from .models import Book, Library
 from django.views.generic.detail import DetailView
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, logout
-from django.contrib import messages
 
 def register(request):
     form = UserCreationForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user) 
+            login(request, user)
             return redirect("list_books")
     return render(request, "relationship_app/register.html", {"form": form})
 
+def login_view(request):
+    form = AuthenticationForm()
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("list_books")
+        else:
+            messages.error(request, "Invalid username or password")
+    return render(request, "relationship_app/login.html", {"form": form})
+
 def logout_view(request):
     logout(request)
-    messages.success(request, 'You have been logged out successfully')
+    messages.success(request, "You have been logged out")
     return redirect("login")
 
 def list_books(request):
