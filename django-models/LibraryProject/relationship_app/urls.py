82a27a613846c23a95d92ce154dd5954d79f9248
David Akinola <dakinola54@gmail.com>
2025-07-18 18:53:56 +0100

    Checker please

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
-from .views import register_view, list_books, LibraryDetailView, CustomLoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import register, logout_view, list_books, LibraryDetailView
 
 urlpatterns = [
-    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),    
-    path("register/", register_view, name="register"),
-    path("login/", CustomLoginView.as_view(), name="login"),
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+    path("register/", register, name="register"),
     path("books/", list_books, name="list_books"),
     path("books/<int:pk>/", LibraryDetailView.as_view(), name="Book detail"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,30 +1,24 @@
 from django.shortcuts import render, redirect
-from .models import Book
-from .models import Library
+from .models import Book, Library
 from django.views.generic.detail import DetailView
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout
 from django.contrib import messages
-from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.views import LoginView as DjangoLoginView
 
-class CustomLoginView(DjangoLoginView):
-    template_name = "relationship_app/login.html"
+def register(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) 
+            return redirect("list_books")
+    return render(request, "relationship_app/register.html", {"form": form})
 
 def logout_view(request):
     logout(request)
     messages.success(request, 'You have been logged out successfully')
     return redirect("login")
-
-def register_view(request):
-    form = UserCreationForm()
-
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save() 
-            return redirect("list_books")
-    return render(request, "relationship_app/register.html", {"form": form})
 
 def list_books(request):
     context = {"books": Book.objects.all()}
@@ -33,4 +27,3 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     template_name = "relationship_app/library_detail.html"
     model = Library
-
