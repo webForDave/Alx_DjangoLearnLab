b509c5e059ba23b3b22a199071e426809336b7d0
David Akinola <dakinola54@gmail.com>
2025-07-18 18:33:22 +0100

    modified code to suit checker's auth

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,25 +2,21 @@ from django.shortcuts import render, redirect
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 
 def login_view(request):
+    form = AuthenticationForm()
+
     if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
             login(request, user)
             return redirect("list_books")
-        else:
-            messages.error(request, "Invalid username pr password")
-            return render(request, "relationship_app/login.html", {})
 
-    return render(request, "relationship_app/login.html", {})
+    return render(request, "relationship_app/login.html", {"form": form})
 
 def logout_view(request):
     logout(request)
@@ -28,34 +24,14 @@ def logout_view(request):
     return redirect("login")
 
 def register_view(request):
+    form = UserCreationForm()
+
     if request.method == 'POST':
-        username = request.POST.get("username") 
-        email = request.POST.get("email")
-        password1 = request.POST.get("password1")
-        password2 = request.POST.get("password2")
-
-        if not username:
-            messages.error(request, "A username must be given")
-            return render(request, "relationship_app/register.html")
-
-        if password1 != password2:
-            messages.error(request, "Passwords do not match")
-            return render(request, "relationship_app/register.html")
-        
-        if User.objects.filter(username=username).exists():
-            messages.error(request, "Username already taken")
-            return render(request, "relationship_app/register.html")
-        
-        if User.objects.filter(email=email).exists():
-            messages.error(request, "Email already registered")
-            return render(request, "relationship_app/register.html")
-        
-        user = User.objects.create_user(username=username, email=email, password=password1)
-        login(request, user)
-        return redirect("list_books")
-
-    else:
-        return render(request, "relationship_app/register.html", {})
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save() 
+            return redirect("list_books")
+    return render(request, "relationship_app/register.html", {"form": form})
 
 def list_books(request):
     context = {"books": Book.objects.all()}
