bf22392a036753e0fb2fbd8d0ed276a5baec8930
David Akinola <dakinola54@gmail.com>
2025-07-19 03:00:22 +0100

    Moved role views into app's view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from .models import Book, Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import user_passes_test, login_required
+from .models import UserProfile
 
 def register(request):
     form = UserCreationForm()
@@ -21,3 +23,27 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     template_name = "relationship_app/library_detail.html"
     model = Library
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+@login_required
+def admin_dashboard(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+@login_required
+def librarian_dashboard(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+@login_required
+def member_dashboard(request):
+    return render(request, 'relationship_app/member_view.html')
